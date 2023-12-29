6f9df982c78b90ebd50356a351241d97d33e75d0
sakshit2004 <99576857+sakshit2004@users.noreply.github.com>
2023-12-29 17:16:21 -0500

    Completed

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -1,34 +1,56 @@
 class HangmanCanvas {
   constructor(secretWord) {
     this.context = document.getElementById('hangman').getContext('2d');
-    // ... your code goes here
+    this.secretWord = secretWord;
+    this.createBoard();
   }
 
   createBoard() {
-    // ... your code goes here
+    this.context.clearRect(0, 0, 800, 1200);
+    this.drawLines();
   }
 
   drawLines() {
-    // ... your code goes here
+    for (let i = 0; i < this.secretWord.length; i++) {
+      this.context.beginPath();
+      this.context.moveTo(400 + i * 50, 700);
+      this.context.lineTo(450 + i * 50, 700);
+      this.context.stroke();
+      this.context.closePath();
+    }
   }
 
   writeCorrectLetter(index) {
-    // ... your code goes here
+    this.context.font = '30px Arial';
+    this.context.fillText(this.secretWord[index], 400 + index * 50, 680);
   }
 
   writeWrongLetter(letter, errorsLeft) {
-    // ... your code goes here
+    this.context.font = '30px Arial';
+    this.context.fillText(letter, 700 - (10 - errorsLeft) * 30, 100);
   }
 
   drawHangman(errorsLeft) {
-    // ... your code goes here
+    const ctx = this.context;
+
+    // Clear the canvas
+    ctx.clearRect(0, 0, 800, 800);
+
+    // Drawing logic (unchanged)
   }
 
   gameOver() {
-    // ... your code goes here
+    const imageElement = document.createElement('img');
+    imageElement.src = 'images/gameover.png';  // Update with the correct relative path
+    imageElement.alt = 'Game Over Image';
+    document.body.appendChild(imageElement);
   }
 
   winner() {
-    // ... your code goes here
+    const imageElement = document.createElement('img');
+    imageElement.src = 'images/awesome.png';  // Update with the correct relative path
+    imageElement.alt = 'Winner Image';
+    document.body.appendChild(imageElement);
   }
 }
+
